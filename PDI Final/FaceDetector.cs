4462b7c395c3bdcc25cfe00c128c758067b08d34
alefESE <alef_berg@hotmail.com.br>
2019-09-23 22:04:42 -0300

    PDI Final add

---
+++ PDI Final/FaceDetector.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using Emgu.CV;
+using System.IO;
+using System;
+
+namespace PDI_Final
+{
+    class FaceDetector
+    {
+        private CascadeClassifier _classifier;
+        private static readonly string _projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+
+        public FaceDetector(string xmlPath)
+        {
+            _classifier = new CascadeClassifier(xmlPath);
+        }
+
+        public Rectangle[] Detect(Mat image)
+        {
+            var scaleFactor = 1.2f;
+            var minNeighbors = 5;
+            var minSize = new Size(30, 30);
+            return _classifier.DetectMultiScale(image, scaleFactor, minNeighbors, minSize);
+        }
+
+        private static List<Mat> CutFaces(Mat image, Rectangle[] facesCoord)
+        {
+            List<Mat> faces = new List<Mat>();
+
+            foreach (var faceCoord in facesCoord)
+            {
+                var w_rm = (int)(0.3 * faceCoord.Width / 2);
+                var roi = new Rectangle(
+                        x: faceCoord.X + w_rm,
+                        y: faceCoord.Y,
+                        width: faceCoord.Width - w_rm,
+                        height: faceCoord.Height
+                        );
+                faces.Add(new Mat(image, roi));
+            }
+
+            return faces;
+        }
+
+        private static List<Mat> Resize(List<Mat> images, Size? size = null)
+        {
+            size = size ?? new Size(224, 224);
+
+            List<Mat> images_norm = new List<Mat>();
+            foreach (var image in images)
+            {
+                var image_norm = new Mat();
+                if (image.Size.Width < size?.Width && image.Size.Height < size?.Height)
+                    CvInvoke.Resize(image, image_norm, size.Value, interpolation: Emgu.CV.CvEnum.Inter.Area);
+                else
+                    CvInvoke.Resize(image, image_norm, size.Value, interpolation: Emgu.CV.CvEnum.Inter.Cubic);
+                images_norm.Add(image_norm);
+            }
+
+            return images_norm;
+        }
+
+        private static List<Mat> NormalizeFaces(Mat image, Rectangle[] facesCoord)
+        {
+            var faces = CutFaces(image, facesCoord);
+            faces = Resize(faces);
+
+            return faces;
+        }
+
+        private static (List<Mat>, string[]) CollectDataset()
+        {
+            var images = new List<Mat>();
+            var labels = new List<string>();
+
+            var people = Directory.GetDirectories(_projectPath + Path.DirectorySeparatorChar + "people")
+                .Select((value, index) => new { index, value })
+                .ToDictionary(pair => pair.index, pair => Path.GetFileName(pair.value));
+
+            foreach (var person in people)
+            {
+                foreach (var image in Directory.GetFiles(_projectPath + Path.DirectorySeparatorChar + "people"
+                    + Path.DirectorySeparatorChar + person.Value))
+                {
+                    images.Add(CvInvoke.Imread(image));
+                    labels.Add(person.Value);
+                }
+            }
+
+            return (images, labels.ToArray());
+        }
+
+        public static void Run(string xmlPath)
+        {
+            (var images, var labels) = CollectDataset();
+
+            if (!Directory.Exists(_projectPath + Path.DirectorySeparatorChar + "people_allign"))
+                Directory.CreateDirectory(_projectPath + Path.DirectorySeparatorChar + "people_allign");
+
+            for (int i = 0; i < images.Count; i++)
+            {
+                var detector = new FaceDetector(xmlPath);
+                var faces_coord = detector.Detect(images[i]);
+                var faces = NormalizeFaces(images[i], faces_coord);
+                for (int j = 0; j < faces.Count; j++)
+                {
+                    if (!Directory.Exists(_projectPath + Path.DirectorySeparatorChar + "people_allign"
+                        + Path.DirectorySeparatorChar + labels[i]))
+                        Directory.CreateDirectory(_projectPath + Path.DirectorySeparatorChar + "people_allign"
+                        + Path.DirectorySeparatorChar + labels[i]);
+
+                    CvInvoke.Imwrite(_projectPath + Path.DirectorySeparatorChar + "people_allign"
+                        + Path.DirectorySeparatorChar + labels[i] + Path.DirectorySeparatorChar
+                        + labels[i] + (i + j) + ".jpg", faces[j]);
+                }
+                    
+            }
+        }
+    }
+}
---
+++ PDI Final/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDI_Final
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Mat _frame;
+        private Mat _gray;
+        private CascadeClassifier _faceClassifier;
+        private readonly string _projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _faceClassifier = new CascadeClassifier(_projectPath + Path.DirectorySeparatorChar + "haarcascade_frontalface_default.xml");
+            _capture = new VideoCapture(0);
+
+            _capture.ImageGrabbed += ProcessFrame;
+            _frame = new Mat();
+            _gray = new Mat();
+            if (_capture != null)
+            {
+                try
+                {
+                    _capture.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+            }
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+                CvInvoke.CvtColor(_frame, _gray, Emgu.CV.CvEnum.ColorConversion.Rgb2Gray);
+                var faces = _faceClassifier.DetectMultiScale(_gray, 1.1, 10, Size.Empty); //the actual face detection happens here
+                foreach (var face in faces)
+                    CvInvoke.Rectangle(_frame, face, new Emgu.CV.Structure.MCvScalar(0, 255, 0), 1);
+                pictureBox1.Image = _frame.Bitmap;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            FaceDetector.Run(_projectPath + Path.DirectorySeparatorChar + "haarcascade_frontalface_default.xml");
+        }
+    }
+}
